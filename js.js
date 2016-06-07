06be2c0f170905513b62d8dfe621a24cac293efe
srini88 <pvsrinivas.ece@gmail.com>
2016-06-07 16:57:52 -0500

    hello world working

---
+++ js.js
@@ -0,0 +1,3 @@
+angular.module('app',[]).controller('HelloWorldCtrl', function($scope){
+	$scope.hellomessage = "Hello World";
+})
---
+++ notes.js
@@ -0,0 +1,58 @@
+controllers and templates..
+
+services 
+
+directives...
+
+As web apps bigger...angularJS for MVC style spa's..
+
+Single responsibility principle...
+
+Services=
+
+Testing ---
+
+what , why
+
+EventReg...angular event registration site....
+
+he course is out of date. Angular 1.5 has released, and although all the code in this course works with 1.5, there is the new feature of components which you should learn about. 
+
+
+Angular - JS framework -- MV* framework -- knockout or backbone...
+
+It is opinioted framework.....do it in a certain way...
+
+Model -- data -- view - render to user ---
+
+controller/presenter/viewModel -- angular uses a controller--
+
+Angular Features -- handles AJAX --- page --- partial templates....2 way data binding --- routing....one view to another -- key in spa's ...angular will also update url in the browser..
+
+also Testability... supports isolated unit tests --- integrating ened-to -end test
+Angular team uses Karma... 
+
+Extends HTML vocabulary...by doing directives...
+teach HTML new tricks..
+
+
+directives -- make display widget...all shit like that...
+
+Angular is forward thinking..
+
+1) Web Componenet s --- truly encapsulate widgets and all that...
+2) Object.observe -- watch an object for changes, and react to those changes...
+
+
+Angular supports - both unit tests and end to end tests..
+
+
+Angular Architecture --- supports two way binding..
+you dont need to watch specific  events and manula update...
+Angular does dirty checking...
+Depency injection -- nice encapsulation...and good for testing..
+
+
+Controllers contain logic and state... views/directives...one way or two way  binding..
+
+services -- 
