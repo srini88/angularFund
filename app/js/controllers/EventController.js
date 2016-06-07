f313d9c8f10642c9a6c373861b277061cf4c9707
srini88 <pvsrinivas.ece@gmail.com>
2016-06-07 17:51:17 -0500

    shwoing google logo working

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -4,6 +4,13 @@ eventsApp.controller('EventsController',
 		$scope.event ={
 			name :'Angular Boot Camp',
 			date: '1/1/2013',
-			time:'10.30am'
+			time:'10.30am',
+			location:{
+				address:'Google headquarters',
+				city:'Mountain View',
+				province :'CA'
+			},
+			imageUrl:'/img/angularjs-logo.png'
+
 		}
 	})
