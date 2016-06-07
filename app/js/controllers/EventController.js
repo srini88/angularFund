92d99745a34131c5c164bd45edaf540e09b13a47
srini88 <pvsrinivas.ece@gmail.com>
2016-06-07 17:38:07 -0500

    working fine event is showing up

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,5 +1,9 @@
 'use strict';
 eventsApp.controller('EventsController',
 	function EventController($scope){
-		
+		$scope.event ={
+			name :'Angular Boot Camp',
+			date: '1/1/2013',
+			time:'10.30am'
+		}
 	})
